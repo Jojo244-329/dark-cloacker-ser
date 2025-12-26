876da6cfadd375d761a839282aaa77c57ef045c3
Jojo244-329 <Jojofag244@proton.me>
2025-12-26 19:44:19 -0300

    ajustar front

--- src/server.js
+++ src/server.js
@@ -23,7 +23,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // 🗂️ Serve arquivos estáticos das pastas white e black
 app.use("/white", express.static(path.join(__dirname, "public", "white")));
-app.use("/black", express.static(path.join(__dirname, "public", "black")));
+app.use(express.static(path.join(__dirname, "public", "black")));
+
 
 // 🛠️ Serve os assets (css, js, fontes) usados nos HTMLs
 app.use("/assets", express.static(path.join(__dirname, "public", "black", "assets")));  // ⬅️ ADICIONA ESTA LINHA
